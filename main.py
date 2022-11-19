8545daf62e95315979572b9b31952f1b9bcd513b
Mosshart <filip.mystek@gmail.com>
2022-11-19 20:17:56 +0100

    uncomment code fix

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ async def StartMain():
     return 'Filip Mystek'
 
 
- @app.get("/auth/{creditnails}")
- async def AuthWithTime(login: str, password:str ):
+@app.get("/auth/{creditnails}")
+async def AuthWithTime(login: str, password:str ):
      return {'time': AfterAuth.returnActualTime(login,password)}
 
- @app.get("/number/{item_id}")
- async def Number(item_id: int):
+@app.get("/number/{item_id}")
+async def Number(item_id: int):
      return {item_id : NumberOperations.IsNumberPrime(item_id)}
 
 
- @app.post("/ImageColorInvert")
- def image_filter(img: UploadFile = File(...)):
+@app.post("/ImageColorInvert")
+def image_filter(img: UploadFile = File(...)):
      return JPGFileOperations.invertImageColors(img)
-
+#
