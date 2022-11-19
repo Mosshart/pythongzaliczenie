11546754845e3578a51004d2eaaaed829106923a
Mosshart <filip.mystek@gmail.com>
2022-11-19 19:54:50 +0100

    heroku test

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ app = FastAPI()
 @app.get("/")
 async def StartMain():
     return 'Filip Mystek'
-
-
-@app.get("/auth/{creditnails}")
-async def AuthWithTime(login: str, password:str ):
-    return {'time': AfterAuth.returnActualTime(login,password)}
-
-@app.get("/number/{item_id}")
-async def Number(item_id: int):
-    return {item_id : NumberOperations.IsNumberPrime(item_id)}
-
-
-@app.post("/ImageColorInvert")
-def image_filter(img: UploadFile = File(...)):
-    return JPGFileOperations.invertImageColors(img)
-
+#
+#
+# @app.get("/auth/{creditnails}")
+# async def AuthWithTime(login: str, password:str ):
+#     return {'time': AfterAuth.returnActualTime(login,password)}
+#
+# @app.get("/number/{item_id}")
+# async def Number(item_id: int):
+#     return {item_id : NumberOperations.IsNumberPrime(item_id)}
+#
+#
+# @app.post("/ImageColorInvert")
+# def image_filter(img: UploadFile = File(...)):
+#     return JPGFileOperations.invertImageColors(img)
+#
